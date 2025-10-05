f5bfdbf3a4c461ddd9722fdf94c342c77dc26ef6
ngkhactung <khactung1611@gmail.com>
2025-10-05 22:13:22 +0700

    Code base BE

---
+++ MDWMS_BE/MilkDistributionWarehouse/MilkDistributionWarehouse/Configurations/DependencyInjectionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using MilkDistributionWarehouse.Repositories;
+using MilkDistributionWarehouse.Services;
+using Online_Learning.Services.Ultilities;
+
+namespace MilkDistributionWarehouse.Configurations
+{
+    public static class DependencyInjectionConfiguration
+    {
+        public static void AddDependencyInjection(this IServiceCollection services)
+        {
+            //User
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserService, UserService>();
+
+            //Email
+            services.AddSingleton<EmailUtility>();
+        }
+    }
+}
---
+++ MDWMS_BE/MilkDistributionWarehouse/MilkDistributionWarehouse/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using MilkDistributionWarehouse.Constants;
+using MilkDistributionWarehouse.Models.Entities;
+
+namespace MilkDistributionWarehouse.Repositories
+{
+    public interface IUserRepository
+    {
+        public User? GetUserById(int userId);
+    }
+
+    public class UserRepository: IUserRepository
+    {
+        private readonly WarehouseContext _context;
+
+        public UserRepository(WarehouseContext context)
+        {
+            _context = context;
+        }
+
+        public User? GetUserById(int userId)
+        {
+            return _context.Users
+                .Where(u => u.UserId == userId && u.Status != CommonStatus.Deleted)
+                .FirstOrDefault();
+        }
+    }
+}
