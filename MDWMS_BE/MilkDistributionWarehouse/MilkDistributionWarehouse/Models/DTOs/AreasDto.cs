06730e19d5e5e609ffa38c80e39c5fb265c17d3b
Nguyen Kim Duong <duongnkhe171819@fpt.edu.vn>
2025-10-13 22:49:30 +0700

    Fix Area

--- MDWMS_BE/MilkDistributionWarehouse/MilkDistributionWarehouse/Models/DTOs/AreasDto.cs
+++ MDWMS_BE/MilkDistributionWarehouse/MilkDistributionWarehouse/Models/DTOs/AreasDto.cs
@@ -17,6 +17,23 @@ namespace MilkDistributionWarehouse.Models.DTOs
             public DateTime? UpdateAt { get; set; }
         }
 
+        public class AreaDetailDto
+        {
+            public int AreaId { get; set; }
+            public string AreaName { get; set; }
+            public string AreaCode { get; set; }
+            public string Description { get; set; }
+            public int StorageConditionId { get; set; }
+            public int Status { get; set; }
+            public decimal TemperatureMin { get; set; }
+            public decimal TemperatureMax { get; set; }
+            public decimal HumidityMin { get; set; }
+            public decimal HumidityMax { get; set; }
+            public string LightLevel { get; set; }
+            public DateTime? CreatedAt { get; set; }
+            public DateTime? UpdateAt { get; set; }
+        }
+
         public class AreaCreateDto
         {
             [Required(ErrorMessage = "Tên khu vực không được để trống")]
