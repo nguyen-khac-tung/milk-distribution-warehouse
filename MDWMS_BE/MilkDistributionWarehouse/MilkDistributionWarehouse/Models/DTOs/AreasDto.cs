1f4ec5e30b6c29c85a123d3bc3bcdeedc9b1dd26
Hoang Dat <99459103+HoangDat33@users.noreply.github.com>
2025-10-13 19:30:46 +0700

    Add storage condition infomation in areaDto

--- MDWMS_BE/MilkDistributionWarehouse/MilkDistributionWarehouse/Models/DTOs/AreasDto.cs
+++ MDWMS_BE/MilkDistributionWarehouse/MilkDistributionWarehouse/Models/DTOs/AreasDto.cs
@@ -13,6 +13,11 @@ namespace MilkDistributionWarehouse.Models.DTOs
             public string Description { get; set; }
             public int StorageConditionId { get; set; }
             public int Status { get; set; }
+            public decimal TemperatureMin { get; set; }
+            public decimal TemperatureMax { get; set; }
+            public decimal HumidityMin { get; set; }
+            public decimal HumidityMax { get; set; }
+            public string LightLevel { get; set; }
             public DateTime? CreatedAt { get; set; }
             public DateTime? UpdateAt { get; set; }
         }
