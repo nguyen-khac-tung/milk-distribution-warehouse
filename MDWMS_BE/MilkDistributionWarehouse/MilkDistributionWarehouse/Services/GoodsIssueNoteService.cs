e2682653d431b4aa7bfd720f205d30bb6b564df1
nvminh2003 <minhnv.works@gmail.com>
2025-11-06 02:57:32 +0700

    fix note BE

--- MDWMS_BE/MilkDistributionWarehouse/MilkDistributionWarehouse/Services/GoodsIssueNoteService.cs
+++ MDWMS_BE/MilkDistributionWarehouse/MilkDistributionWarehouse/Services/GoodsIssueNoteService.cs
@@ -50,8 +50,8 @@ namespace MilkDistributionWarehouse.Services
 
             if (salesOrder.AssignTo != userId) return "Người dùng hiện tại không được phân công cho đơn hàng này.".ToMessageForUser();
 
-            //if (salesOrder.EstimatedTimeDeparture > DateOnly.FromDateTime(DateTime.Now))
-            //    return "Không tạo được phiếu xuất kho trước ngày dự kiến xuất kho.".ToMessageForUser();
+            if (salesOrder.EstimatedTimeDeparture > DateOnly.FromDateTime(DateTime.Now))
+                return "Không tạo được phiếu xuất kho trước ngày dự kiến xuất kho.".ToMessageForUser();
 
             if (salesOrder.Status != SalesOrderStatus.AssignedForPicking)
                 return "Chỉ có thể tạo phiếu xuất kho cho đơn hàng ở trạng thái 'Đã phân công'.".ToMessageForUser();
